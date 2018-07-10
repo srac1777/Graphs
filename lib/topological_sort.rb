b03db681578876ea0814ed4f92f0e289c96eb2cb
Shashank Racherla <shashank.racherla@gmail.com>
2018-07-10 10:33:45 -0700

    third commit

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -3,5 +3,20 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+    queue = Queue.new
+
+    vertices.each do |v|
+        if v.in_edges.empty?
+            queue.enq(v)
+        end
+    end   
+
+    sorted = []
+    until queue.empty?
+        current = queue.pop
+        sorted << current
+
+        
+    end
 
 end
